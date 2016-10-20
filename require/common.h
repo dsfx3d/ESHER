f72b3870baaada1f431b837d6fcdde013e0c55cd
dsfx3d <dsfx3d@gmail.com>
2016-10-20 16:33:06 +0530

    D1: Test Git Bash

---
+++ require/common.h
@@ -0,0 +1,17 @@
+#ifndef COMMON_dsfx3d
+#defineCOMMON_dsfx3d
+
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <string.h>
+
+using namespace std;
+
+#define loop(m) for(int i=0; i<m; i++)
+#define loop2(i,m) for(int i=0; i<m; i++)
+#define loop3(i,m,n) for(int i=m; i<=n; i++)
+
+#endif
